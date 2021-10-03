5884266679b9301b1132161d075af95221de03b7
Osama Idrees <51856701+OsamaIdrees@users.noreply.github.com>
2021-10-03 21:36:49 +0500

    update todolist

--- index.js
+++ index.js
@@ -52,6 +52,20 @@ app.get("/delete-todo-task/:task_id",function(req,res){
         }
     })
 })
+app.post("/update-task",function(req,res){
+    const task_id = req.body.id
+    const task = req.body.task
+    const status = req.body.status
+    const deadline = req.body.deadline
+    db.query(`update todolist set task = '${task}', status = '${status}' , deadline = '${deadline}' where id = '${task_id}'`,function(err,result){
+        if(err){
+            return res.json({'status':false,'message':err})
+        }
+        else{
+            return res.json({'status':true,'message':'Task Updated Successfully'})
+        }
+    })
+})
 app.get("*",function(req,res){
     res.send("Sorry! Route does not exist")
 })
