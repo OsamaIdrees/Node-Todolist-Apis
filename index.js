845e758ca388a3fa025ba2aa125ce16488ce2e05
Osama Idrees <51856701+OsamaIdrees@users.noreply.github.com>
2021-10-04 00:47:17 +0500

    Register and Login Api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors")
 const db = require('./db_connection');
+const e = require("express");
 app.use(cors());
 app.use(express.urlencoded({
     extended:true
@@ -63,6 +64,42 @@ app.post("/update-task",function(req,res){
         }
         else{
             return res.json({'status':true,'message':'Task Updated Successfully'})
+        }
+    })
+})
+app.post('/register',function(req,res){
+    const user_information = {
+        username:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        contact_no:req.body.contact_no
+    }
+    db.query(`insert into user(username,email,password,contact_no) values('${user_information.username}','${user_information.email}','${user_information.password}','${user_information.contact_no}')`,function(err,result){
+        if(err){
+            console.log(err)
+        }
+        else{
+            return res.json({'status':true,'message':'User Registered Successfully'});
+        }
+    })
+})
+app.post('/login',function(req,res){
+    const loginInfo = {
+        email:req.body.email,
+        password:req.body.password
+    }
+    db.query(`select id,username,email,contact_no from user where email = '${loginInfo.email}' and password = '${loginInfo.password}'`,function(err,result){
+        if(err){
+            console.log(err)
+        }
+        else{
+            if(result.length !== 0){
+                return res.json({'status':true,'message':'Login Successful', 'information':result});
+            }
+            else{
+                return res.json({'status':false,'message':'Invalid email or password'});
+            }
+          
         }
     })
 })
