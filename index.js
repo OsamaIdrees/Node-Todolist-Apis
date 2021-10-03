763629aab9704814fb6704ea2e727a29a6176b9d
Osama Idrees <51856701+OsamaIdrees@users.noreply.github.com>
2021-10-03 20:15:37 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const cors = require("cors")
+const db = require('./db_connection');
+app.use(cors());
+app.use(express.urlencoded({
+    extended:true
+}));
+app.use(express.json())
+app.get("/get-all-tasks",function(req,res){
+   db.query("select * from todolist",function(err,result){
+       if(err){
+           console.log(err)
+       }
+       else{
+           return res.json(result)
+       }
+   })
+});
+app.post("/add-todo-item",function(req,res){
+    const task = req.body.task
+    const status = req.body.status
+    const deadline = req.body.deadline
+    db.query(`insert into todolist (task,status,deadline) values ('${task}','${status}','${deadline}') `,function(err,result){
+        if(err){
+            console.log(err)
+        }
+        else{
+            return res.json({'status':true,'message':'Task Added'});
+        }
+    })
+})
+
+app.get("/delete-todo-task/:task_id",function(req,res){
+    const task_id = req.params.task_id
+    db.query(`delete from todolist where id = ${task_id}`,function(err,result){
+        if(err){
+            return res.json({'status':false,'message':err})
+        }
+        else{
+            if(result.affectedRows > 0){
+                return res.json({'status':true,'message':'Task deleted Successfully'})
+            }
+            else{
+                return res.json({'status':false,'message':"Something went wrog"})
+            }
+            
+            
+        }
+    })
+})
+app.listen("5000")
